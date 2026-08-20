1e7f8134cc04e2782c96d0e8b5474b7bc6b554f6
Stanisław Bożych <stachubozych@gmail.com>
2026-08-20 13:34:13 +0200

    fix

--- frontend/server.ts
+++ frontend/server.ts
@@ -1,16 +1,18 @@
-import { AngularNodeAppEngine, createNodeRequestHandler, isMainModule, writeResponseToNodeResponse } from '@angular/ssr/node';
+import { CommonEngine } from '@angular/ssr/node';
 import express from 'express';
-import { dirname, resolve } from 'node:path';
+import { dirname, resolve, join } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import bootstrap from './src/main.server';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
+const indexHtml = join(serverDistFolder, 'index.server.html');
 
 const app = express();
-const angularApp = new AngularNodeAppEngine();
+const commonEngine = new CommonEngine();
 
 /**
- * Serve static files from /browser with long-term caching.
+ * Serve static files from /browser
  */
 app.use(
   express.static(browserDistFolder, {
@@ -23,26 +25,24 @@ app.use(
 /**
  * Handle all other requests by rendering the Angular application.
  */
-app.use('/**', (req, res, next) => {
-  angularApp
-    .handle(req)
-    .then((response) =>
-      response ? writeResponseToNodeResponse(response, res) : next(),
-    )
-    .catch(next);
+app.use((req, res, next) => {
+  const { protocol, originalUrl, headers } = req;
+
+  commonEngine
+    .render({
+      bootstrap,
+      documentFilePath: indexHtml,
+      url: `${protocol}://${headers.host}${originalUrl}`,
+      publicPath: browserDistFolder,
+    })
+    .then((html) => res.send(html))
+    .catch((err) => next(err));
 });
 
-/**
- * Start the server if this module is the main entry point.
- */
-if (isMainModule(import.meta.url)) {
-  const port = process.env['PORT'] || 4000;
-  app.listen(port, () => {
-    console.log(`Angular SSR server listening on http://localhost:${port}`);
-  });
-}
+const port = process.env['PORT'] || 4000;
+app.listen(port, () => {
+  console.log(`Angular SSR server listening on http://localhost:${port}`);
+});
 
-/**
- * Request handler used by the Angular CLI (dev-server and during build).
- */
-export default createNodeRequestHandler(app);
+export default app;
+
